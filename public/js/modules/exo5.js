9665c72b577003ae68cd99b721ff1193011a06e6
Nathan Van Dyck <vandyck.nathan@gmail.com>
2020-01-27 15:10:46 +0100

    exo5

--- public/js/modules/exo5.js
+++ public/js/modules/exo5.js
@@ -1,15 +1,13 @@
 let images
 export default images = () => {
-    let supprime = (e) => {
-        if (e.currentTarget.parentElement.lastChild != e.currentTarget) {
-            e.currentTarget.parentElement.removeChild(e.currentTarget.parentElement.lastChild);
-        }
-    }
     //IMAGES MULT
     let magic = document.getElementById('magic');
     let supp = document.getElementById('clear');
+    let supLast = document.getElementById('supLast');
     let section = document.getElementById('tab5');
+    let counter = document.getElementById('compteur5');
     let gallery,img;
+    let i = 0;
 
     let addImg = () => {
         if (section.lastElementChild.tagName != "DIV") {
@@ -22,8 +20,25 @@ export default images = () => {
         img.setAttribute("src","./public/img/Catastrophe.gif");
         img.classList.add('m-1');
         gallery.appendChild(img);
+        counter.innerHTML = ++i;
+        console.log(i);
+    }
+
+    let removeLastImage = () => {
+        gallery.lastChild.remove();
+        counter.innerHTML = --i;
+        console.log(i);
+    }
+
+    let supprime = (e) => {
+        if (e.currentTarget.parentElement.lastChild != e.currentTarget) {
+            e.currentTarget.parentElement.removeChild(e.currentTarget.parentElement.lastChild);
+        }
+        i = 0;
+        counter.innerHTML = i;
     }
 
     magic.addEventListener('click',addImg);
+    supLast.addEventListener('click',removeLastImage);
     supp.addEventListener('click',supprime);
 }
