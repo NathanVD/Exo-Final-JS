0509dff161096d71290ae8feb7b67c6912df3779
Nathan Van Dyck <vandyck.nathan@gmail.com>
2020-01-28 09:51:42 +0100

    to do : filtres complétées/à faire/all

--- public/js/modules/exo7.js
+++ public/js/modules/exo7.js
@@ -1,15 +1,41 @@
 //LISTE
 let liste
+
+let haveClass = (element,classe) => {
+    let classes = element.classList;
+    for (let i = 0; i < classes.length; i++) {
+        if (classes[i] == classe) {
+            // si l'élément a la classe cherchée, renvoie true
+            return true
+        } 
+    }
+}
+
 export default liste = () => {
     let input = document.getElementById('input');
     let create = document.getElementById('create');
     let rem = document.getElementById('rem');
     let listZone = document.getElementById('list');
+    let allFilter = document.getElementById('all');
+    let doneFilter = document.getElementById('done');
+    let doingFilter = document.getElementById('doing');
+
+
     let list;
 
     let supprime = () => {
         let cible = document.getElementsByTagName("UL");
         cible[0].remove();
+    }
+
+    let validateLi = (e) => {
+        if (e.target.checked) {
+            e.target.parentElement.classList.remove('bg-info','doing');
+            e.target.parentElement.classList.add('done','bg-success');
+        } else {
+            e.target.parentElement.classList.remove('done','bg-success');
+            e.target.parentElement.classList.add('bg-info','doing');
+        }
     }
 
     let supprimeLi = (e) => {
@@ -46,12 +72,18 @@ export default liste = () => {
         if (input.value != "") {
             input.style.border = ""
             let li = document.createElement("li");
-            li.classList.add("w-50","d-flex","justify-content-end","bg-info","m-1","border")
+            li.classList.add("w-50","d-flex","justify-content-end","bg-info","m-1","border","toDoItem","doing")
             list.appendChild(li);
             let text = document.createElement("p");
             text.classList.add("m-1","mr-auto")
             text.innerHTML = input.value;
             li.appendChild(text);
+            let approve = document.createElement("input");
+            approve.style.height = "32px";
+            approve.style.width = "32px";
+            approve.setAttribute("type","checkbox");
+            li.appendChild(approve);
+            approve.addEventListener('change',validateLi);
             let edit = document.createElement("button");
             edit.innerHTML = '<i class="fas fa-edit"></i>'
             li.appendChild(edit);
@@ -67,6 +99,45 @@ export default liste = () => {
         }
     }
 
+    let displayAll = () => {
+        for (let i = 0; i < toDoItem.length; i++) {
+            toDoItem[i].classList.remove("d-none");
+            if (haveClass(toDoItem[i],"d-flex") != true) {
+                toDoItem[i].classList.add("d-flex");
+            }
+        }
+    }
+
+    let displayDone = () => {
+        for (let i = 0; i < done.length; i++) {
+            done[i].classList.remove("d-none");
+            if (haveClass(done[i],"d-flex") != true) {
+                done[i].classList.add("d-flex");
+            }
+        }
+        for (let i = 0; i < doing.length; i++) {
+            doing[i].classList.remove("d-flex");
+            if (haveClass(doing[i],"d-none") != true) {
+                doing[i].classList.add("d-none");
+            }
+        }
+    }
+
+    let displayDoing = () => {
+        for (let i = 0; i < doing.length; i++) {
+            doing[i].classList.remove("d-none");
+            if (haveClass(doing[i],"d-flex") != true) {
+                doing[i].classList.add("d-flex");
+            }
+        }
+        for (let i = 0; i < done.length; i++) {
+            done[i].classList.remove("d-flex");
+            if (haveClass(done[i],"d-none") != true) {
+                done[i].classList.add("d-none");
+            }
+        }
+    }
+
     create.addEventListener('click', createList);
     input.addEventListener('keypress', () => { 
         if (event.keyCode == 13) {
@@ -74,4 +145,10 @@ export default liste = () => {
         }
     });
     rem.addEventListener('click',supprime);
+    let done = document.getElementsByClassName('done');
+    let doing = document.getElementsByClassName('doing');
+    let toDoItem = document.getElementsByClassName('toDoItem');
+    allFilter.addEventListener('click',displayAll);
+    doneFilter.addEventListener('click',displayDone);
+    doingFilter.addEventListener('click',displayDoing);
 }
